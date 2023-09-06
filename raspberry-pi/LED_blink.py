3dd62c1502038580f31e5b6952dca328dba52eaf
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-09-06 10:47:11 -0400

    adada

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -1,7 +1,8 @@
-import board# type: ignore
+# type: ignore
+import board
 import digitalio# type: ignore
 from time import sleep
-led = digitalio.DigitalInOut(board.GP0)
+led = digitalio.DigitalInOut(board.led)
 led.direction = digitalio.Direction.OUTPUT
 print("hello")
 
---
+++ raspberry-pi/launch.py
@@ -0,0 +1,8 @@
+# type: ignore
+import board
+import digitalio
+from time import sleep
+
+for i in range 0,10,-1:
+    print(i)
+    sleep(1)
