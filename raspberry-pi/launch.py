853e4f3762323c07babc9607f427705f06d1bd67
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-09-07 10:26:44 -0400

    Part 2

--- raspberry-pi/launch.py
+++ raspberry-pi/launch.py
@@ -3,6 +3,18 @@ import board
 import digitalio
 from time import sleep
 
-for i in range 0,10,-1:
+Rled = digitalio.DigitalInOut(board.GP2) # Fourth pin down, top left
+Rled.direction = digitalio.Direction.OUTPUT
+Gled = digitalio.DigitalInOut(board.GP0) # First pin, top left
+Gled.direction = digitalio.Direction.OUTPUT
+
+
+for i in range (10,0,-1): #start, stop, step; nice and clean
     print(i)
-    sleep(1)
+    Rled.value = True # Flash red
+    sleep(.1)
+    Rled.value = False
+    sleep(.9)
+print("Liftoff")
+Gled.value = True
+sleep(5)
