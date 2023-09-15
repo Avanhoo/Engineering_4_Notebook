a9cc0527e092b4323cc55514edc207aa8c67edc5
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-09-15 10:44:00 -0400

    broken

---
+++ raspberry-pi/IMU.py
@@ -0,0 +1,33 @@
+import board
+import busio
+import adafruit_mpu6050
+from time import sleep
+
+sda_pin = board.GP16
+scl_pin = board.GP17
+i2c = busio.I2C(scl_pin, sda_pin)
+imu = adafruit_mpu6050.MPU6050(i2c)
+x = 0
+y = 0
+z = 0
+xO = 0
+yO = 0
+zO = 0
+rX = 0
+rY = 0
+rZ = 0
+delay = .1
+
+sleep(delay)
+xO = -(imu.acceleration[0])
+yO = -(imu.acceleration[1])
+zO = -(imu.acceleration[2])
+print (xO, yO, zO)
+sleep(delay)
+
+while True:
+    x += (imu.acceleration[0] + xO)*delay
+    y += (imu.acceleration[1] + yO)*delay
+    z += (imu.acceleration[2] + zO)*delay
+    print(f"X: {x} Y: {y} Z: {z}")
+    sleep(delay)
