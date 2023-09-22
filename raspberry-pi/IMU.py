e436c11b567c1350c5b4ececf34d43d7fb3df47d
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-09-22 10:46:22 -0400

    IMU changes

--- raspberry-pi/IMU.py
+++ raspberry-pi/IMU.py
@@ -7,47 +7,34 @@ import displayio
 from time import sleep
 import adafruit_displayio_ssd1306
 from adafruit_display_text import label
+displayio.release_displays() # Necesarry or else the GPIO pin will still be seen as in use even after the code is done
 
-digitalio.DigitalInOut(board.GP17).deinit()
 
 i2c = busio.I2C(board.GP17, board.GP16)
-displayio.release_displays()
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP20) # Display
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 imu = adafruit_mpu6050.MPU6050(i2c, address=0x68) # Accelerometer
 
 led = digitalio.DigitalInOut(board.GP3)
 led.direction = digitalio.Direction.OUTPUT
-sda_pin = board.GP16
-scl_pin = board.GP17
 
-x = 0
-y = 0
-z = 0
 delay = .1
 
 sleep(delay)
 
 while True:
-    print(f"Accel: {round(imu.acceleration[0]-.6,1)}, {round(imu.acceleration[1]+.2,1)}, {round(imu.acceleration[2],1)}")
-    if abs(imu.acceleration[0]-.6) > 9.3 or abs(imu.acceleration[1]+.2) > 9.3:
+    print(f"Accel: {round(imu.acceleration[0]-.6,1)}, {round(imu.acceleration[1]+.2,1)}, {round(imu.acceleration[2],1)}") # Uses fstring to place the rounded acceleration values into the string
+    if abs(imu.acceleration[0]-.6) > 9.3 or abs(imu.acceleration[1]+.2) > 9.3 or (imu.acceleration[2] < 0): # Detects if the imu is tilted 90° in any direction
         led.value = True
     else:
         led.value = False
 
-    # create the display group
-    splash = displayio.Group()
-
-    # add title block to display group
-    title = "ANGULAR VELOCITY"
-    # the order of this command is (font, text, text color, and location)
-    text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
+    splash = displayio.Group() # create the display group
+    title = f"ANGULAR VELOCITY: \n X:{round(imu.gyro[0]-.6,3)} \n Y:{round(imu.gyro[1]+.2,3)} \n Z:{round(imu.gyro[2],3)}" # add title block to display group
+    
+    text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5) # the order of this command is (font, text, text color, and location)
     splash.append(text_area)    
-
-    # you will write more code here that prints the x, y, and z angular velocity values to the screen below the title. Use f strings!
-    # Don't forget to round the angular velocity values to three decimal places
-
-    # send display group to screen
-    display.show(splash)
+    
+    display.show(splash)# send display group to screen
 
     sleep(delay)
