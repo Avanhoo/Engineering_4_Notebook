9c6f4b89708080d1610af431a0500613d6e16251
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-10-26 10:52:36 -0400

    Graph stuff

--- raspberry-pi/coordinates.py
+++ raspberry-pi/coordinates.py
@@ -27,31 +27,31 @@ def area():
     c1 = [0,0]
     c2 = [0,0]
     c3 = [0,0]
-    
+
+    r1 = input("Coordinate 1: ")
     try:                    # Coordinate 1
-        r1 = input("Coordinate 1: ")
         c1 = [int(o) for o in r1.split(",")] # Splits raw string: "1,2" into a string array: "1", "2", and turns each value into an int: 1,2
     except:
-        print("Coordinate 1 Invalid, please enter in 'x,y' format")
-        pass
+        print("Invalid Coordinate 1 , please enter in 'x,y' format")
+        continue
     finally:
-
         qdisp(f"Coordinates: \n c1: {c1}")
+        
+        r2 = input("Coordinate 2: ")
         try:                # Coordinate 2
-            r2 = input("Coordinate 2: ")
             c2 = [int(o) for o in r2.split(",")]
         except:
-            print("Coordinate 2 Invalid, please enter in 'x,y' format")
-            pass
+            print("Invalid Coordinate 2, please enter in 'x,y' format")
+            continue
         finally:
-
             qdisp(f"Coordinates: \n c1: {c1} \n c2: {c2}")
+
+            r3 = input("Coordinate 3: ")
             try:            # Coordinate 3
-                r3 = input("Coordinate 3: ")
                 c3 = [int(o) for o in r3.split(",")]
             except:
-                print("Coordinate 3 Invalid, please enter in 'x,y' format")
-                pass
+                print("Invalid Coordinate 3 , please enter in 'x,y' format")
+                continue
 
             finally:
                 A = (1/2)*abs(c1[x]*(c2[y] - c3[y]) + c2[x]*(c3[y] - c1[y]) + c3[x]*(c1[y] - c2[y])) # Easy plug and play equation for a triangle's area
