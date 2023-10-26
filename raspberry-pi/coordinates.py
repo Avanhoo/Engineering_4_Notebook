d7e51d161faed4615c21ae1549440cb34efa596a
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-10-26 10:52:46 -0400

    Graph

--- raspberry-pi/coordinates.py
+++ raspberry-pi/coordinates.py
@@ -7,6 +7,11 @@ import displayio
 from time import sleep
 import adafruit_displayio_ssd1306
 from adafruit_display_text import label
+from adafruit_display_shapes.triangle import Triangle
+from adafruit_display_shapes.line import Line
+from adafruit_display_shapes.circle import Circle
+
+
 displayio.release_displays() # Necesarry or else the GPIO pin will still be seen as in use even after the code is done
 
 i2c = busio.I2C(board.GP17, board.GP16)
@@ -23,47 +28,67 @@ def qdisp(title): # Stands for quick display. Creates a function that runs all t
     splash.append(text_area)    
     display.show(splash)# send display group to screen
 
+def qgraph(c1,c2,c3):
+    scale = 1
+    x0 = 64
+    y0 = 32
+    splash = displayio.Group()
+
+    hline = Line(0,y0,128,y0, color=0xFFFF00)
+    vline = Line(x0,0,x0,64, color=0xFFFF00)
+    splash.append(hline) 
+    splash.append(vline)
+
+    triangle = Triangle((c1[x]+x0)*scale, (c1[y]+y0)*scale, (c2[x]+x0)*scale, (c2[y]+y0)*scale, (c3[x]+x0)*scale, (c3[y]+y0)*scale, outline=0xFFFF00)
+    splash.append(triangle)
+
+
+    display.show(splash)
+
 def area():
-    c1 = [0,0]
-    c2 = [0,0]
-    c3 = [0,0]
+    c1 = []
+    c2 = []
+    c3 = []
 
-    r1 = input("Coordinate 1: ")
-    try:                    # Coordinate 1
-        c1 = [int(o) for o in r1.split(",")] # Splits raw string: "1,2" into a string array: "1", "2", and turns each value into an int: 1,2
-    except:
-        print("Invalid Coordinate 1 , please enter in 'x,y' format")
-        continue
-    finally:
-        qdisp(f"Coordinates: \n c1: {c1}")
-        
-        r2 = input("Coordinate 2: ")
-        try:                # Coordinate 2
-            c2 = [int(o) for o in r2.split(",")]
-        except:
-            print("Invalid Coordinate 2, please enter in 'x,y' format")
-            continue
-        finally:
-            qdisp(f"Coordinates: \n c1: {c1} \n c2: {c2}")
+    while (c1 or c2 or c3) == []:
+        r1 = input("Coordinate 1: ")
+        try:                    # Coordinate 1
+            c1 = [int(o) for o in r1.split(",")] # Splits raw string: "1,2" into a string array: "1", "2", and turns each value into an int: 1,2
+            a = c1[0] + c1[1]
+            qdisp(f"Coordinates: \n c1: {c1}")
 
-            r3 = input("Coordinate 3: ")
-            try:            # Coordinate 3
-                c3 = [int(o) for o in r3.split(",")]
+            r2 = input("Coordinate 2: ")
+            try:                # Coordinate 2
+                c2 = [int(o) for o in r2.split(",")]
+                qdisp(f"Coordinates: \n c1: {c1} \n c2: {c2}")
+
+                r3 = input("Coordinate 3: ")
+                try:            # Coordinate 3
+                    c3 = [int(o) for o in r3.split(",")]
+                    A = (1/2)*abs(c1[x]*(c2[y] - c3[y]) + c2[x]*(c3[y] - c1[y]) + c3[x]*(c1[y] - c2[y])) # Easy plug and play equation for a triangle's area
+                    qdisp(f" c1: {c1} \n c2: {c2} \n c3: {c3} \nArea: {A}") # add title block to display group
+                    return A
+                    qgraph(c1,c2,c3)
+
+                except:
+                    print("Invalid Coordinate 3 , please enter in 'x,y' format")
+                    continue
+                    
             except:
-                print("Invalid Coordinate 3 , please enter in 'x,y' format")
+                print("Invalid Coordinate 2, please enter in 'x,y' format")
                 continue
-
-            finally:
-                A = (1/2)*abs(c1[x]*(c2[y] - c3[y]) + c2[x]*(c3[y] - c1[y]) + c3[x]*(c1[y] - c2[y])) # Easy plug and play equation for a triangle's area
-                qdisp(f"c1: {c1} \n c2: {c2} \n c3: {c3} \n Area: {A}") # add title block to display group
-
-
                 
+        except:
+            print("Invalid Coordinate 1 , please enter in 'x,y' format")
+            continue
 
-                return A
+            
+
+
 
 
 
 
 while True:
-    print(area())
+    qgraph([1,1],[1,8],[5,5])
+    #print(f"Area = {area()}")
