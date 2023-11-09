d3d7e8df18b661a20f07c8bd5c9e0421a52d65eb
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2023-11-09 10:49:42 -0500

    Added libs

--- raspberry-pi/data_storage.py
+++ raspberry-pi/data_storage.py
@@ -2,7 +2,7 @@ import board
 import busio
 import adafruit_mpu6050
 import digitalio
-from time import sleep
+from time import sleep, monotonic
 
 led = digitalio.DigitalInOut(board.GP3)
 led.direction = digitalio.Direction.OUTPUT
@@ -12,18 +12,23 @@ sda_pin = board.GP16
 scl_pin = board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
 imu = adafruit_mpu6050.MPU6050(i2c)
-x = 0
-y = 0
-z = 0
 
-delay = .1
+delay = .15
+print(monotonic())
+with open("/data.csv", "a") as datalog:
+    while True:
+        print(f"Accel: {round(imu.acceleration[0]-.6,1)}, {round(imu.acceleration[1]+.2,1)}, {round(imu.acceleration[2],1)}") # Prints the acceleration
+        if abs(imu.acceleration[0]-.6) > 9.3 or abs(imu.acceleration[1]+.2) > 9.3:
+            led.value = True
+            tilt = 1
+        else:
+            led.value = False
+            tilt = 0
 
-sleep(delay)
+        datalog.write(f"{monotonic()},{imu.acceleration[0]},{imu.acceleration[0]},{imu.acceleration[0]},{tilt}\n") # Writes the time, x, y, z acceleration, and if tilted to a file 
+        datalog.flush()
 
-while True:
-    print(f"Accel: {round(imu.acceleration[0]-.6,1)}, {round(imu.acceleration[1]+.2,1)}, {round(imu.acceleration[2],1)}")
-    if abs(imu.acceleration[0]-.6) > 9.3 or abs(imu.acceleration[1]+.2) > 9.3:
-        led.value = True
-    else:
-        led.value = False
-    sleep(delay)
+        sled.value = True # Flashes onboard LED
+        sleep(delay/2)
+        sled.value = False
+        sleep(delay/2)
