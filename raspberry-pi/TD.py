0ae8aa366d452649d601c66a707b53ca61b329b0
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2024-01-02 20:16:38 -0500

    a

--- raspberry-pi/TD.py
+++ raspberry-pi/TD.py
@@ -10,6 +10,7 @@ from adafruit_display_shapes.line import Line
 from adafruit_display_shapes.circle import Circle
 from adafruit_display_shapes.rect import Rect'''
 from math import cos, sin, tan
+import pygame
 
 '''displayio.release_displays() # Necesarry or else the GPIO pin will still be seen as in use even after the code is done
 i2c = busio.I2C(board.GP17, board.GP16)
@@ -21,11 +22,15 @@ pX = 0
 pY = 0
 pr = 0
 
-rows, cols = (64,128)
-map = [[0]*cols]*rows
-map[1][0] = 1
+cols, rows = (2,64)
+map = [[0]*cols]*rows # Creates an array of 128 x 64
 disp = [[0]*cols]*rows
-print(map)
+
+pygame.init()
+screen = pygame.display.set_mode((128,64))
+clock = pygame.time.Clock()
+running = True
+
 
 def refresh():
     for yray in range(1,64): # Up to down
@@ -37,11 +42,25 @@ def refresh():
                 yval += sin(pr-(xray/2)+xray)
                 if map[int(xval)][int(yval)] == 1:
                     disp[int(xval)][int(yval)] = 1
-splash = displayio.Group()
-while True:
-    inp = input("-")
-    if inp == "a":
-        pr += 15
-    elif inp == "d":
-        pr -= 15
-                
+
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    pygame.image.frombuffer(map, (cols, rows), "RGB")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
