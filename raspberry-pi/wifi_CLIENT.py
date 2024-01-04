cc6896b66d631abd622a2f33b39da2de0afcc86d
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2024-01-04 10:48:35 -0500

    server work!

--- raspberry-pi/wifi_CLIENT.py
+++ raspberry-pi/wifi_CLIENT.py
@@ -2,16 +2,35 @@
 import board
 import wifi
 import socketpool
+from time import sleep
 from adafruit_httpserver import Server, Request, Response
 
 ssid="avanhooPi"
 passwd="piinthesky"
-
 print(f"Connecting to {ssid}")
 while True: # Waits for connection before proceeding
     try:
         wifi.radio.connect(ssid, passwd)
-    finally:
+    except:
+        print("trying...")
+    
+    if wifi.radio.connected == True:
         print(f"Connected to {ssid}")
+        print(wifi.radio.ipv4_address)
         break
+    sleep(1)
 
+
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, "/static", debug=True)
+
+
+@server.route("/")
+def base(request: Request):
+    
+    #Serve a default static plain text message.
+    return Response(request, "Instructions")
+
+
+server.serve_forever(str(wifi.radio.ipv4_address))
+# https://docs.circuitpython.org/projects/httpserver/en/latest/examples.html#id1
--- raspberry-pi/wifi_HOST.py
+++ raspberry-pi/wifi_HOST.py
@@ -15,16 +15,7 @@ wifi.radio.start_station()
 print(f"gateway: {wifi.radio.ipv4_gateway_ap}")
 ip = ipaddress.ip_address("192.168.4.16")
 
-pool = socketpool.SocketPool(wifi.radio)
-server = Server(pool, "/static", debug=True)
-
-
-@server.route("/")
-def base(request: Request):
-    
-    #Serve a default static plain text message.
-    return Response(request, "Instructions")
-
-
-server.serve_forever(str(wifi.radio.ipv4_address))
-# https://docs.circuitpython.org/projects/httpserver/en/latest/examples.html#id1
+while True:
+    time.sleep(1)
+    print("running...")
+    print(wifi.radio.ping(wifi.radio.ipv4_gateway_ap))
