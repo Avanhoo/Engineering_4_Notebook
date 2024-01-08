55f0a095baaf6c0e22abc5919b4c7bb38a814027
Avanhoo <avanhoo2498@charlottesvilleschools.org>
2024-01-07 19:59:54 -0500

    i2c is the future screw wifi

--- raspberry-pi/i2c_test.py
+++ raspberry-pi/i2c_test.py
@@ -33,3 +33,15 @@ def recieve_commands():
     aileron = arr[0] # Separates array into variables
     elevator = arr[1]
     return aileron, elevator
+
+mode = 0 # Only necesarry for test file
+while mode == 0:
+    aileron = -89
+    elevator = 37
+    send_commands(aileron, elevator)
+    print("Sent data")
+    sleep(.5)
+while mode == 1:
+    recieve_commands()
+    print(f"Recieved \nAileron: {aileron}\nElevator: {elevator}")
+    sleep(.5)
